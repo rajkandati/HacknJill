4bd202a58f0d0512f009e9e1e634be55a0a29fcc
raj kandathi <raj.kandati@gmail.com>
2012-06-16 10:44:02 -0400

    Code for HacknJill hackathon

---
+++ HacknJill/NYTimesAPIKeys.h
@@ -0,0 +1,5 @@
+
+
+//New Times API keys
+
+#define NYTimesEventsAPIKey @"ee1d0601677813adfc0504e902040833:17:66236751"
---
+++ HacknJill/NyTimesData.h
@@ -0,0 +1,24 @@
+//
+//  NyTimesData.h
+//  HacknJill
+//
+//  Created by Raj Kandati on 6/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NyTimesData : NSObject
+
+
+//Returns the top categories
+- (NSArray *)getCategories;
+
+
+//Returns the sub categories
+- (NSArray *)getSubCategories;
+
+
+
+
+@end
