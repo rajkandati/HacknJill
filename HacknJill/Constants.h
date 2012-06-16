e29d33e59779fce70fead56d46704a63e738bbce
raj kandathi <raj.kandati@gmail.com>
2012-06-16 10:31:04 -0400

    Constants used in the project

---
+++ HacknJill/Constants.h
@@ -0,0 +1,13 @@
+
+
+// Constants used in the project
+
+#define RESPONSE_TYPE_JASON @"json"
+#define RESPONSE_TYPE_XML @"xml"
+#define RESPONSE_TYPE_SPHP @"sphp"
+
+
+//Keys in NY Times dataset
+#define CATEGORIES_KEY @"category"
+#define SUBCATEGORIES_KEY @"subcategory"
+#define FACETS_KEY @"facets"
